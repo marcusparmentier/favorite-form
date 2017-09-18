f1faba85a064268c5785310591c76e83fa91bc30
Marcus Parmentier <marcusjparmentier@gmail.com>
2017-09-18 11:49:00 -0700

    Add requested array and put into the last li in the ul tag.

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,17 @@ $(function() {
     var namef = $("input#nameInput").val();
     var animalf = $("input#animalInput").val();
     var numberf = $("input#numberInput").val();
-    var arrayf = [colorf, namef, animalf, numberf];
+    var favs = [colorf, namef, animalf, numberf];
 
-    $(".favArray").text(arrayf);
+    // var selElements = favs.slice(0, 3);
+
+    var selElements = [namef, colorf, animalf];
+    var selElements2 = [];
+
+    selElements2.push(selElements);
+
+    $(".favArray").text(favs);
+    $(".testArray").text(selElements2);
 
     $(".favColor").text(colorf);
     $(".favName").text(namef);
